cad952d5ac64b782e939e5f5e6f16b952f4cee68
qiaozp <chivalry.pp@gmail.com>
2021-04-10 20:13:30 +0800

    Basic view now returns minimal HTML

--- test/test_django_install.py
+++ test/test_django_install.py
@@ -10,10 +10,10 @@ class NewVisitorTest(unittest.TestCase):
         pass
         self.browser.quit()
 
-    # def test_can_start_a_list_and_retrieve_it_later(self):
-    #     self.browser.get('http://localhost:8000')
-    #     self.assertIn('To-Do',self.browser.title)
-    #     self.fail("Test is finish")
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do',self.browser.title)
+        self.fail("Test is finish")
 
 
 if __name__ == '__main__':
