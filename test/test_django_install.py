5b209e57a7ad1748d9ffe97cec3faba70f8ecdb8
qiaozp <chivalry.pp@gmail.com>
2021-04-10 12:13:05 +0800

    Initial commit

---
+++ test/test_django_install.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self) -> None:
+        pass
+        # self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        assert 'install' in self.browser.title, "the browser's title is " + self.browser.title
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
