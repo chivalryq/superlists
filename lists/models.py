6bb328f8e89c80c4e006567eb9ae5375701ec50a
qiaozp <chivalry.pp@gmail.com>
2021-05-22 23:27:57 +0800

    Modify models

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 
-# Create your models here.
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
     # 没有长度限制的字符串类型字段
     text = models.TextField(default="")
+    list = models.ForeignKey(List, on_delete=models.CASCADE, default=None)
