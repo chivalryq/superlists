f0e6edbbeb41d43f1a196bbf9a21764a1ca38784
qiaozp <chivalry.pp@gmail.com>
2021-05-22 22:22:16 +0800

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -12,10 +12,7 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
 
-        html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
         self.assertTemplateUsed(response, 'home.html')
-        self.assertEqual(html, expected_html)
 
     def test_can_save_a_POST_request(self):
         self.client.post('/', data={'item_text': 'A new list item'})
@@ -27,20 +24,11 @@ class HomePageTest(TestCase):
     def test_redircets_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_display_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        response = self.client.get('/')
-
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -62,3 +50,19 @@ class ItemModelTest(TestCase):
 
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_display_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
